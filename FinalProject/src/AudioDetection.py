253020ad1df716aa5d8dae480c4805c105dbba72
Jet Webb <jetwebb1@gmail.com>
2024-10-26 21:41:27 +1100

    Moved to Version2

--- FinalProject/src/AudioDetection.py
+++ FinalProject/src/AudioDetection.py
@@ -1,5 +1,13 @@
+#LEGACY FILE - LEGACY FILE - LEGACY FILE - LEGACY FILE - LEGACY FILE - LEGACY FILE - LEGACY FILE - LEGACY FILE - LEGACY FILE - LEGACY FILE - LEGACY FILE - LEGACY FILE
+#LEGACY FILE - LEGACY FILE - LEGACY FILE - LEGACY FILE - LEGACY FILE - LEGACY FILE - LEGACY FILE - LEGACY FILE - LEGACY FILE - LEGACY FILE - LEGACY FILE - LEGACY FILE
+#LEGACY FILE - LEGACY FILE - LEGACY FILE - LEGACY FILE - LEGACY FILE - LEGACY FILE - LEGACY FILE - LEGACY FILE - LEGACY FILE - LEGACY FILE - LEGACY FILE - LEGACY FILE
+#LEGACY FILE - LEGACY FILE - LEGACY FILE - LEGACY FILE - LEGACY FILE - LEGACY FILE - LEGACY FILE - LEGACY FILE - LEGACY FILE - LEGACY FILE - LEGACY FILE - LEGACY FILE
+#LEGACY FILE - LEGACY FILE - LEGACY FILE - LEGACY FILE - LEGACY FILE - LEGACY FILE - LEGACY FILE - LEGACY FILE - LEGACY FILE - LEGACY FILE - LEGACY FILE - LEGACY FILE
+
+
+
 #This node subscribes to /audio topic, compiles multiple audio segments then analyses them, comparing to a reference audio signal
-#Also checks for too high or too low sound
+#Also checks for too high or too low sound volume
 
 
 # AUDIO sources https://pixabay.com/sound-effects/search/explosion/
@@ -10,7 +18,7 @@
 
 #       tried but suck
 # https://github.com/ros-drivers/audio_common/tree/ros2 (link)
-# git clone https://github.com/ros-drivers/audio_common.git -b ros2 (do this instead)
+# git clone https://github.com/ros-drivers/audio_common.git -b ros2 
 # sudo apt-get install libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev libgstreamer-plugins-bad1.0-dev gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav gstreamer1.0-tools gstreamer1.0-x gstreamer1.0-alsa gstreamer1.0-gl gstreamer1.0-gtk3 gstreamer1.0-qt5 gstreamer1.0-pulseaudio
 
 
@@ -33,7 +41,7 @@
 
 
 
-#       INSTALL TESTING
+#       INSTALL TESTING (to see if it works)
 
 # ros2 run audio_common audio_player_node
 # ros2 run audio_common music_node
@@ -62,7 +70,7 @@ from audio_common_msgs.msg import AudioStamped
 from audio_common.utils import msg_to_array
 import matplotlib.pyplot as plt
 import noisereduce as nr
-from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import ThreadPoolExecutor, Future
 import threading
 
 #TODO Play around with samplerate, k value, threshold, comparison time to get it running smoothly and more reliably
@@ -75,12 +83,17 @@ class AudioComparisonNode(Node):
         self.lock = threading.Lock()
         self.threshold = 1000
         self.setSampleRate = 4300*2
-        self.compareSizeDivider = 1
-        self.hop_length = 256 #fidelity / resolution of cross similarity map
+        self.compareSizeDivider = 2
+        self.hop_length = 128 #fidelity / resolution of cross similarity map
         self.counter = 0
+        self.counter2 = 0
         self.maxSoundVol = 2000
         self.minSoundVol= 0.00001 #in a silent room my microphone reads about 0.00025
         self.thresholdVolume = 500
+        self.current_task: Future = None
+        self.n_fft = 1024
+        self.comparison_done = False  # Flag to track if comparison is done for current graph
+        self.savedSubbedAudio = np.zeros(10)
         # File Loading
         self.reference_data, self.ref_sr = librosa.load('/home/jet/ros2_ws/src/audio_common/audio_common/samples/explosion.mp3', sr=self.setSampleRate)
         self.get_logger().info(f"Loaded reference audio with sample rate {self.setSampleRate}")
@@ -93,8 +106,7 @@ class AudioComparisonNode(Node):
         self.minLength = int(len(self.liveAudioBuffer)/self.compareSizeDivider) #Snippet size to compare audio signals
         self.refAudio = self.reference_data[:self.minLength]
         #Plotting
-        self.fig, self.ax = plt.subplots(nrows=5, sharex=True, gridspec_kw={'height_ratios': [1, 1, 1, 1, 4]}
-)
+        self.fig, self.ax = plt.subplots(nrows=6, sharex=True, gridspec_kw={'height_ratios': [1, 1, 1, 1, 4, 4]})
         self.ax[0].set(title='Sample Audio File')
         self.ax[1].set(title='Live Audio')
         self.ax[2].set(title='Live Audio Snippet')
@@ -109,20 +121,22 @@ class AudioComparisonNode(Node):
 
         plt.ion()
         plt.show()
+        chroma_ref = librosa.feature.chroma_stft(y=self.refAudio, sr=self.setSampleRate, hop_length=self.hop_length, n_chroma=12, n_fft=self.n_fft)
 
-        chroma_ref = librosa.feature.chroma_cqt(y=self.refAudio, sr=self.setSampleRate, hop_length=self.hop_length, n_chroma=12)
+        # chroma_ref = librosa.feature.chroma_cqt(y=self.refAudio, sr=self.setSampleRate, hop_length=self.hop_length, n_chroma=12)
         self.x_ref = librosa.feature.stack_memory(chroma_ref, n_steps=2, delay=3)
 
 
         # Create a subscription to the audio topic
         qos_profile = qos_profile_sensor_data
         self.sub = self.create_subscription(AudioStamped, "/audio", self.audio_callback, qos_profile)
-
+        self.timer = self.create_timer((self.sampleTime/self.compareSizeDivider), self.sample_time_callback) #call timer every x seconds
         self.get_logger().info("AudioComparison node started")
 
 
 
-
+    def sample_time_callback(self):
+        self.threads.submit(self.run_audio_comparison)   
 
     def audio_callback(self, msg: AudioStamped) -> None:
         # Convert the received audio message to a NumPy array
@@ -131,46 +145,51 @@ class AudioComparisonNode(Node):
         if subbedAudio is None:
             self.get_logger().error(f"Format {msg.audio.info.format} unknown")
             return
-            
-
-
-        self.subbedSRRate = msg.audio.info.rate
-        # subbedAudio = nr.reduce_noise(y=subbedAudio, sr=msg.audio.info.rate, prop_decrease=1, n_jobs=1) #opt
-
+        
+        subbedAudio = nr.reduce_noise(y=subbedAudio, sr=msg.audio.info.rate, prop_decrease=1) #opt
 
         # Convert audio data to float and normalize
-        liveAudio = subbedAudio.astype(np.float32)
-        if np.max(liveAudio) > self.maxSoundVol:
-            self.get_logger().info(f"Loud Environment {np.max(liveAudio): .10f} Max Sound Recorded")
-        if np.mean(liveAudio) < self.minSoundVol and np.mean(liveAudio) > 0: #idk why but mic reading goes - sometimes
-            self.get_logger().info(f"Average sound level of {np.mean(liveAudio): .10f} is unusually low, please test microphone")
-        liveAudio = librosa.util.normalize(liveAudio, threshold=self.thresholdVolume, fill=False)
-        # Append live audio to the buffer and discard the oldest data
-        buffer_size = len(self.liveAudioBuffer)
-        liveAudioSize = len(liveAudio)
+
+        self.liveAudio = subbedAudio.astype(np.float16)
+        print("byte size",(self.liveAudio.nbytes))
+
+        self.liveAudio = librosa.util.normalize(self.liveAudio, threshold=self.thresholdVolume, fill=False)
+
+        self.volumeCheck() #graph gets stuck if audio comparison is on same thread
+
+        if len(self.liveAudio[self.liveAudio != 0]) > 1000: # if there is a non empty message
+            # Append live audio to the buffer and discard the oldest data
+            liveAudioSize = len(self.liveAudio)
+            
+            if liveAudioSize > len(self.liveAudioBuffer):
+                self.liveAudioBuffer = self.liveAudio[-len(self.liveAudioBuffer):]
+            else:
+                self.liveAudioBuffer = np.roll(self.liveAudioBuffer, -liveAudioSize) #shifts whole buffer left
+                self.liveAudioBuffer[-liveAudioSize:] = self.liveAudio #put new chunk of audio on teh end of buffer
+
+
+            # if len(self.liveAudioBuffer[self.liveAudioBuffer != 0]) >= self.minLength:
+
+            #         # thready.result()
+            #         # self.run_audio_comparison(self.liveAudio)
         
-        if liveAudioSize > buffer_size:
-            self.liveAudioBuffer = liveAudio[-buffer_size:]
-        else:
-            self.liveAudioBuffer = np.roll(self.liveAudioBuffer, -liveAudioSize)
-            self.liveAudioBuffer[-liveAudioSize:] = liveAudio
+            self.liveAudio = self.liveAudioBuffer[:self.minLength]
 
+        # if self.liveAudio
 
-        
+        # if self.counter == 0 and len(self.liveAudio[self.liveAudio != 0]) > 100: #checks there is actual audio within the message
 
-
-        self.liveAudio = self.liveAudioBuffer[:self.minLength]
-
-
-        # Adjust lengths for comparison
-        if self.counter > 7:
-            self.counter = 0
-
-        if self.counter == 0 and len(self.liveAudio[self.liveAudio != 0]) > 100: #checks there is actual audio within the message
-            self.threads.submit(self.run_audio_comparison, self.liveAudio) #graph gets stuck if audio comparison is on same thread
-        self.counter+=1
+        # self.counter+=1
+        print("byte size2",(self.liveAudio.nbytes))
 
         self.plotWaveforms()
+
+    def volumeCheck(self):
+        
+        if np.max(self.liveAudio) > self.maxSoundVol:
+            self.get_logger().info(f"Loud Environment {np.max(self.liveAudio): .10f} Max Sound Recorded")
+        if np.mean(self.liveAudio) < self.minSoundVol and np.mean(self.liveAudio) > 0: #idk why but mic reading goes - sometimes
+            self.get_logger().info(f"Average sound level of {np.mean(self.liveAudio): .10f} is unusually low, please test microphone")
 
     def plotWaveforms(self):
 
@@ -180,22 +199,35 @@ class AudioComparisonNode(Node):
         self.ax[1].set(title='Live Audio')
         # plt.pause(0.01)  # Pause to allow the plot to update, it freaks out lower than this
 
-
         #Snippet of live audio
-        self.ax[2].cla()  # Clear the second plot (live snippet audio)
-        librosa.display.waveshow(self.liveAudio, sr=self.setSampleRate, ax=self.ax[2])
-        self.ax[2].set(title='Live Audio Snippet')
+        if len(self.liveAudio[self.liveAudio != 0]) > 100: #checks there is actual audio within the message
+            self.ax[2].cla()  # Clear the second plot (live snippet audio)
+            librosa.display.waveshow(self.liveAudio, sr=self.setSampleRate, ax=self.ax[2])
+            self.ax[2].set(title='Live Audio Snippet')
+
         try:
             librosa.display.specshow(self.xsim, x_axis='s', y_axis='s', cmap='magma_r', hop_length=self.hop_length, ax=self.ax[4])
         except Exception as e:
             self.get_logger().error(f"Error in audio comparison: {str(e)}")
         plt.pause(0.01)  # Pause to allow the plot to update, it freaks out lower than this
 
+
+
+        try:
+            self.ax[5].cla()  # Clear the second plot (live snippet audio)
+            # librosa.display.waveshow(self.DIAGNOSTIC, sr=self.setSampleRate, ax=self.ax[5])
+            librosa.display.specshow(self.DIAGNOSTIC, x_axis='s', y_axis='s', cmap='magma_r', hop_length=self.hop_length, ax=self.ax[5])
+            # plt.plot(np.linspace(0,self.setSampleRate, len(self.DIAGNOSTIC)), self.DIAGNOSTIC)
+            # plt.show()
+            # self.ax[5].set(title='DIAGNOSTIC')
+        except Exception as e:
+            self.get_logger().error(f"diag: {str(e)}")
+
 #THREAD START
 
-    def run_audio_comparison(self, liveAudio: np.ndarray) -> None:
+    def run_audio_comparison(self) -> None:
         try: 
-            similarity = self.compare_audio(liveAudio)
+            similarity = self.compare_audio()
 
             if similarity > self.threshold:
                 self.get_logger().info(f"Audio matches with similarity {similarity:.2f} (above threshold)")
@@ -205,61 +237,42 @@ class AudioComparisonNode(Node):
             self.get_logger().error(f"Error in audio comparison: {str(e)}")
 
 
-    def compare_audio(self, liveAudio: np.ndarray) -> float:
+    def compare_audio(self) -> float:
+        if len(self.liveAudio[self.liveAudio != 0]) > 1000: # if there is a non empty message
 
-        min_len = min(len(liveAudio), len(self.refAudio))
-        liveAudio = liveAudio[:min_len]
-        # self.get_logger().info(f"Reference audio length: {len(refAudio)}, Live audio length: {len(liveAudio)}")
-        # self.get_logger().info(f"Live audio sample: {liveAudio[:10]}")
-        # self.get_logger().info(f"Reference audio sample: {refAudio[:10]}")
+            # self.liveAudio, self.ref_sr = librosa.load('/home/jet/ros2_ws/src/audio_common/audio_common/samples/explosion.mp3', sr=self.setSampleRate, offset=0) #use to test if function is working
+            self.DIAGNOSTIC = np.fft.fft(self.liveAudio)
+            self.DIAGNOSTIC = np.abs(self.DIAGNOSTIC)
+            min_len = min(len(self.liveAudio), len(self.refAudio))
+            self.liveAudio = self.liveAudio[:min_len]
 
-        #NEW CODE
+            # self.get_logger().info(f"Reference audio length: {len(refAudio)}, Live audio length: {len(liveAudio)}")
+            # self.get_logger().info(f"Live audio sample: {liveAudio[:10]}")
+            # self.get_logger().info(f"Reference audio sample: {refAudio[:10]}")
 
-        chroma_comp = librosa.feature.chroma_cqt(y=liveAudio, sr=self.setSampleRate, hop_length=self.hop_length, n_chroma=12)
+            #NEW CODE
+
+            # chroma_comp = librosa.feature.chroma_cqt(y=liveAudio, sr=self.setSampleRate, hop_length=self.hop_length, n_chroma=12)
 
 
-        # chroma_ref = librosa.feature.chroma_stft(y=refAudio, sr=self.setSampleRate, hop_length=self.hop_length, n_chroma=12, n_fft=128)
-        # chroma_comp = librosa.feature.chroma_stft(y=liveAudio, sr=self.setSampleRate, hop_length=self.hop_length, n_chroma=12, n_fft=128)
+            chroma_comp = librosa.feature.chroma_stft(y=self.liveAudio, sr=self.setSampleRate, hop_length=self.hop_length, n_chroma=12, n_fft=self.n_fft)
+            self.DIAGNOSTIC = chroma_comp
 
 
-        # Use time-delay embedding to get a cleaner recurrence matrix
-        x_comp = librosa.feature.stack_memory(chroma_comp, n_steps=2, delay=3)
+            # Use time-delay embedding to get a cleaner recurrence matrix
+            x_comp = librosa.feature.stack_memory(chroma_comp, n_steps=2, delay=1)
 
-        #NEW CODE
-        # similarity = np.correlate(x_comp, self.x_ref,"full")
+            #NEW CODE
+            # similarity = np.correlate(x_comp, self.x_ref,"full")
 
-        self.xsim = librosa.segment.cross_similarity(x_comp, self.x_ref, mode='affinity', metric='cosine')        
-
-        # self.get_logger().info(str(similarity))
-        similarity = np.max(self.xsim)
+            self.xsim = librosa.segment.cross_similarity(x_comp, self.x_ref, mode='affinity', metric='cosine')        
+            # self.get_logger().info(str(similarity))
+            similarity = np.sum(self.xsim)
 
         return similarity
     
 
 
-
-
-    #speedy but dodgy way
-
-
-    # from sklearn.metrics.pairwise import cosine_similarity
-    # # Normalize both audio signals
-
-    # # Ensure lengths are the same
-    # min_len = min(len(liveAudio), len(refAudio))
-    # liveAudio = liveAudio[:min_len]
-    # refAudio = refAudio[:min_len]
-    
-    # # Reshape for cosine similarity (requires 2D arrays)
-    # liveAudio = liveAudio.reshape(1, -1)
-    # refAudio = refAudio.reshape(1, -1)
-    
-    # # Compute cosine similarity
-    # similarity = cosine_similarity(liveAudio, refAudio)[0][0] * 1000  # Scaling for easier thresholding
-    
-    # self.get_logger().info(f"Cosine similarity: {similarity}")
-    
-    # return similarity
 #THREAD END
 
 
