81ddbd3351b7aef1ff567ce7bcd19989bc22d65c
JoeUTS <joseph.a.tarbath@student.uts.edu.au>
2024-10-29 10:54:43 +1100

    Launch file working

--- k10/launch/k10.launch.py
+++ k10/launch/k10.launch.py
@@ -18,33 +18,38 @@ def generate_launch_description():
     y_pose = LaunchConfiguration('y_pose', default='-9')
     
     # launch parameters
-    use_sim_time = LaunchConfiguration('use_sim_time', default='true')
-    use_lifestyle_autostart = LaunchConfiguration('autostart', default='true')
-    use_map_subscribe = LaunchConfiguration('map_subscribe_transient_local', default='false')
-    lifecycle_nodes = ['map_server', 'amcl']
+    useSimTime = LaunchConfiguration('use_sim_time', default='true')
+    slam = LaunchConfiguration('slam', default='true')
     
     # filepaths
-    robotLaunch = get_package_share_directory('turtlebot3_gazebo')
-    nav2Launch = get_package_share_directory('nav2_bringup')
+    robotPackageDir = get_package_share_directory('turtlebot3_gazebo')
+    nav2Dir = get_package_share_directory('nav2_bringup')
+    gazeboDir = get_package_share_directory('gazebo_ros')
+    k10Dir = get_package_share_directory('k10')
     world = os.path.join(
-        get_package_share_directory('k10'),
+        k10Dir,
         'world',
         'disaster.world'
     )
-    rviz_config_file = os.path.join(
-        get_package_share_directory('k10'),
+    rvizConfigFile = os.path.join(
+        k10Dir,
         'rviz',
-        'test_config.rviz'
+        'config.rviz'
     )
-    slam_params_file = os.path.join(
+    nav2Params = os.path.join(
+        k10Dir,
+        'config',
+        'nav2_params.yaml'
+    )
+    slamParams = os.path.join(
         get_package_share_directory('k10'),
         'config',
-        'my_ma_mapper_params.yaml'
+        'mapper_params_online_async.yaml'
     )
-    map_file = os.path.join(
-        get_package_share_directory('k10'),
+    mapFile = os.path.join(
+        k10Dir,
         'map',
-        'my_ma.yaml'
+        'map.yaml'
     )
 
     # nodes
@@ -52,7 +57,6 @@ def generate_launch_description():
         package="k10",
         executable="k10",
         name="HQ",
-        arguments=["exec", "HQ"]
     )
     
     AudioDetectionV2 = Node(
@@ -81,80 +85,68 @@ def generate_launch_description():
         executable="rviz2",
         name="rviz2",
         output="log",
-        arguments=["-d", rviz_config_file]
+        arguments=["-d", rvizConfigFile]
     )
     
     # navigation / localisation
-    slam_node = Node(
+    slamNode = Node(
         package='slam_toolbox',
         executable='async_slam_toolbox_node',
         name='slam_toolbox',
         output='screen',
-        parameters=[slam_params_file, {'use_sim_time': use_sim_time}]
+        parameters=[slamParams, {'use_sim_time': useSimTime}]
     )
-
-    map_server = Node(
-        package='nav2_map_server',
-        executable='map_server',
-        name='map_server',
-        output='screen',
-        parameters=[{'yaml_filename': map_file},
-                    {'use_sim_time': use_sim_time}]
-    )
-
-    amcl = Node(
-        package='nav2_amcl',
-        executable='amcl',
-        name='amcl',
-        output='screen',
-        parameters=[{'map': map_file},
-                    {'use_sim_time': use_sim_time}]
-    )
-
+    
     nav2 = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(nav2Launch, 'launch', 'navigation_launch.py')
-        ),
-        launch_arguments={'map_subscribe_transient_local': use_map_subscribe,
-                          'use_sim_time': use_sim_time
+        PythonLaunchDescriptionSource([nav2Dir, '/launch/bringup_launch.py']),
+        launch_arguments={
+            'map' : mapFile,
+            'use_sim_time': useSimTime,
+            'params_file' : nav2Params
         }.items()
     )
-
-    lifecycle_manager_cmd = Node(
-        package='nav2_lifecycle_manager',
-        executable='lifecycle_manager',
-        name='lifecycle_manager',
-        output='screen',
-        emulate_tty=True,  # fixes terminal output https://github.com/ros2/launch/issues/188
-        parameters=[{'use_sim_time': use_sim_time},
-                    {'autostart': use_lifestyle_autostart},
-                    {'node_names': lifecycle_nodes}])
 
     # gazebo
     gzserver_cmd = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
-            os.path.join(get_package_share_directory('gazebo_ros'), 'launch', 'gzserver.launch.py')
+            os.path.join(gazeboDir,
+                         'launch',
+                         'gzserver.launch.py'
+            )
         ),
-        launch_arguments={'world': world}.items()
+        launch_arguments={
+            'world': world
+        }.items()
     )
 
     gzclient_cmd = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
-            os.path.join(get_package_share_directory('gazebo_ros'), 'launch', 'gzclient.launch.py')
+            os.path.join(gazeboDir,
+                         'launch',
+                         'gzclient.launch.py'
+            )
         )
     )
 
     # robot
     robot_state_publisher_cmd = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
-            os.path.join(robotLaunch, 'launch', 'robot_state_publisher.launch.py')
+            os.path.join(robotPackageDir,
+                         'launch',
+                         'robot_state_publisher.launch.py'
+            )
         ),
-        launch_arguments={'use_sim_time': use_sim_time}.items()
+        launch_arguments={
+            'use_sim_time': useSimTime
+        }.items()
     )
 
     spawn_turtlebot_cmd = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
-            os.path.join(robotLaunch, 'launch', 'spawn_turtlebot3.launch.py')
+            os.path.join(robotPackageDir,
+                         'launch',
+                         'spawn_turtlebot3.launch.py'
+            )
         ),
         launch_arguments={
             'x_pose': x_pose,
@@ -167,15 +159,12 @@ def generate_launch_description():
     ld.add_action(gzclient_cmd)
     ld.add_action(spawn_turtlebot_cmd)
     ld.add_action(robot_state_publisher_cmd)
-    ld.add_action(HQNode)
-    ld.add_action(AudioDetectionV2)
-    ld.add_action(personDetection)
-    ld.add_action(WorkingGas)
+    ld.add_action(slamNode)
+    #ld.add_action(HQNode)
+    #ld.add_action(AudioDetectionV2)
+    #ld.add_action(personDetection)
+    #ld.add_action(WorkingGas)
     ld.add_action(rvizNode)
-    ld.add_action(slam_node)
     ld.add_action(nav2)
-    ld.add_action(map_server)
-    ld.add_action(amcl)
-    ld.add_action(lifecycle_manager_cmd)
-
+    
     return ld
--- k10/setup.py
+++ k10/setup.py
@@ -1,27 +1,60 @@
 import os
 from glob import glob
 from setuptools import find_packages, setup
+from ament_index_python.packages import get_package_share_directory
 
 package_name = 'k10'
 
 setup(
     name=package_name,
     version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    packages=find_packages(),
     data_files=[
         ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+        ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'launch'),
-            glob('launch/*.launch.py')),
-        (os.path.join('share', package_name, 'config'),
-            glob('config/*.yaml')),
-        (os.path.join('share', package_name, 'map'),
-            glob('map/*')),
-        (os.path.join('share', package_name, 'world'),
-            glob('world/*.world')),
-        (os.path.join('share', package_name, 'rviz'),
-            glob('rviz/*.rviz'))
+        (os.path.join(
+                'share', 
+                package_name, 
+                'launch'
+                ),
+            glob('launch/*.launch.py')
+        ),
+        (os.path.join(
+                'share',
+                package_name,
+                'config'
+                ),
+            glob('config/*.yaml')
+        ),
+        (os.path.join(
+                'share',
+                package_name,
+                'map'
+                ),
+            glob('map/*')
+        ),
+        (os.path.join(
+                'share',
+                package_name,
+                'world'
+                ),
+            glob('world/*.world')
+        ),
+        (os.path.join(
+                'share',
+                package_name,
+                'rviz'
+                ),
+            glob('rviz/*.rviz')
+        ),
+        (os.path.join(
+                'share',
+                'nav2_bringup',
+                'launch'
+                ),
+            glob('launch/*.launch.py')
+        )
     ],
     install_requires=['setuptools', 'nav2'],
     zip_safe=True,
